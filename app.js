3eea651f4c7838dcfc6d5761cf5665ee826fb040
ahmed-yahya-94 <71339218+ahmed-yahya-94@users.noreply.github.com>
2020-09-28 17:47:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,15 @@ var app = express();
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = 5034
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
